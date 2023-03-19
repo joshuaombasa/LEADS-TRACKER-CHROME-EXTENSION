c70a89328fb7ee70a75cbc6d860b322604f79be3
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-03-19 23:40:12 +0300

    updated sccript file

--- UPDATED LEADS TRACKER EXTENSION/index.js
+++ UPDATED LEADS TRACKER EXTENSION/index.js
@@ -34,23 +34,25 @@ function HandleLeads() {
         localStorage.setItem("myLeads", JSON.stringify(myLeads) )
         render(myLeads)
     }
+
+    this.function render(leads) {
+        let listItems = ""
+        for (let i = 0; i < leads.length; i++) {
+            listItems += `
+                <li>
+                    <a target='_blank' href='${leads[i]}'>
+                        ${leads[i]}
+                    </a>
+                </li>
+            `
+        }
+        ulEl.innerHTML = listItems
+    }
 }
 
 tabBtn.addEventListener("click", sortLead.collectDatafromCorrectTab)
 
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems
-}
+
 
 deleteBtn.addEventListener("dblclick",sortLead.clearAllLeads)
 
